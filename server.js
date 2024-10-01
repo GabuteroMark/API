a50c9a605bc6978b299f8d8f9c940e919783973f
Mark Gabutero <markgabutero17@gmail.com>
2024-10-01 14:12:29 +0800

    HHAHAHAHA

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const errorHandler = require('_middleware/error-handler');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 //const authenticateToken = require('_middleware/authenticateToken');
 //const authorize = require('_middleware/authorize');
 
 
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(cors({origin:(origin,callback) => callback(null, true), credentials: true}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +29,10 @@ app.use('/api/users', require('./users/users.controller'));
 //app.use('/api/auth', require('./users/users.controller'));
 
 app.use('/api/orders', require('./order/order.controller'));
+
+app.use('/accounts', require('./accounts/accounts.controller'));
+
+app.use('/api-docs', require('_helpers/swagger'));
 
 app.use(errorHandler);
 
